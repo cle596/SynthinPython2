a80b3f78b0bdf823cc58217c7fa5e9b62cf9590d
changhan <changhanlee@gmail.com>
2016-06-03 15:35:15 -0400

    f

--- gaudio.py
+++ gaudio.py
@@ -5,7 +5,7 @@ PyAudio = pyaudio.PyAudio
 
 bitrate = 16000
 
-freq = 880
+freq = 440
 length = 1
 
 if freq > bitrate:
@@ -17,7 +17,7 @@ wavedata = ''
 
 for x in range(0, numberofframes):
     wavedata = wavedata + \
-        chr(int(math.sin(x / ((bitrate / freq) / math.pi)) * 127 + 128))
+        chr(int(math.sin(x / ((bitrate / freq) / (2 * math.pi)))*127 + 128))
 
 for x in range(0, restframes):
     wavedata = wavedata + chr(128)
