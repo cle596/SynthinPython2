9b0b60ae0900c81eba1110e65118224f13f5815a
Changhan Lee <Changhan Lee>
2016-06-03 12:42:36 -0400

    button to play sound

--- audio.py
+++ audio.py
@@ -6,7 +6,7 @@ PyAudio = pyaudio.PyAudio
 #See http://en.wikipedia.org/wiki/Bit_rate#Audio
 BITRATE = 16000 #number of frames per second/frameset.
 
-FREQUENCY = 500 #Hz, waves per second, 261.63=C4-note.
+FREQUENCY = 700 #Hz, waves per second, 261.63=C4-note.
 LENGTH = 1 #seconds to play sound
 
 if FREQUENCY > BITRATE:
@@ -23,12 +23,3 @@ for x in range(0,RESTFRAMES):
  WAVEDATA = WAVEDATA+chr(128)
 
 p = PyAudio()
-stream = p.open(format = p.get_format_from_width(1),
-                channels = 1,
-                rate = BITRATE,
-                output = True)
-
-stream.write(WAVEDATA)
-stream.stop_stream()
-stream.close()
-p.terminate()
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import tkinter as tk
 import pprint as pp
+from audio import *
 
 class Application(tk.Frame):
 
     def __init__(self, master=None):
-        tk.Frame.__init__(self, master)
+        tk.Frame.__init__(
+            self,
+            master
+        )
+        master.minsize(width=300, height=300)
         self.pack()
         self.createWidgets()
 
@@ -33,7 +38,16 @@ class Application(tk.Frame):
 
     def play(self):
         print("playing music!")
+        stream = p.open(format = p.get_format_from_width(1),
+                        channels = 1,
+                        rate = BITRATE,
+                        output = True)
+        stream.write(WAVEDATA)
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
 
 root = tk.Tk()
+root.wm_title("Giorgio Synth Looper")
 app = Application(master=root)
 app.mainloop()
