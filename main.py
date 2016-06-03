eb67c914c52b8b345a2c979a03bebe4f974b0acb
changhan <changhanlee@gmail.com>
2016-06-03 13:40:06 -0400

    separating root window from app frame

--- main.py
+++ main.py
@@ -1,53 +1,7 @@
 import tkinter as tk
 import pprint as pp
 from gaudio import *
-
-class Application(tk.Frame):
-
-    def __init__(self, master=None):
-        tk.Frame.__init__(
-            self,
-            master
-        )
-        self.bind("<Button-1>", self.callback)
-        self.pack(fill=tk.BOTH, expand=True)
-        self.createWidgets()
-
-    def callback(self,event):
-        print ("clicked at", event.x, event.y)
-
-    def createWidgets(self):
-        self.PLAY = tk.Button(
-            self,
-            borderwidth=1,
-            relief="ridge",
-            bg="white",
-            font=("Arial", 10)
-        )
-        self.PLAY["text"] = "play"
-        self.PLAY["command"] = self.play
-        self.PLAY.pack()
-
-        self.QUIT = tk.Button(
-            self,
-            text="quit",
-            command=root.destroy,
-            borderwidth=1,
-            relief="ridge",
-            bg="white",
-            font=("Arial", 10)
-        )
-        self.QUIT.pack()
-
-    def play(self):
-        stream = p.open(format=p.get_format_from_width(1),
-                        channels=1,
-                        rate=BITRATE,
-                        output=True)
-        stream.write(WAVEDATA)
-        stream.stop_stream()
-        stream.close()
-        p.terminate()
+from gapp import *
 
 root = tk.Tk()
 w = 600  # width for the Tk root
